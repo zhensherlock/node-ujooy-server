7d3a53c9172826b4a400054da309e49c4c6cbf77
MichaelSun <zhensherlock@163.com>
2017-03-25 16:49:34 +0800

    add index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 /**
  * Created by sunzhenxuan on 2017/3/24.
  */
-var serialPort = require("serialport");
-serialPort.list(function (err, ports) {
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/tty-usbserial1',{
+    baudrate: 57600
+});
+
+port.on('open', function() {
+    port.write('main screen turn on', function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written');
+    });
+});
+
+// open errors will be emitted as an error event
+port.on('error', function(err) {
+    console.log('Error: ', err.message);
+})
+
+
+SerialPort.list(function (err, ports) {
     ports.forEach(function(port) {
-        console.log(port.comName);
-        console.log(port.pnpId);
-        console.log(port.manufacturer);
+        console.log('comName'+port.comName);
+        console.log('pnpId'+port.pnpId);
+        console.log('manufacturer'+port.manufacturer);
     });
 });
